a5530f4167b38b74bab1bba3f2b8fa51da8fe5cc
Alexandre <alexandrepj13@gmail.com>
2022-09-21 14:41:55 -0300

    Arrumando bug do card principal

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,12 @@
 const users = [];
-
 const fetchUser = document.querySelector(".buscar")
-
 fetchUser.addEventListener("click", async () => {
-
     const userName = document.querySelector(".user").value;
     const response = await fetch(`https://api.github.com/users/${userName}`);
     const user = await response.json();
-
     if (user.message) {
-
         console.log("Usuario não encontrado");
     } else {
-
         // Campos para preenchimento
         const loginUser = document.querySelector(".login-user")
         const nameUser = document.querySelector(".name-user")
@@ -32,30 +26,22 @@ fetchUser.addEventListener("click", async () => {
         document.querySelector(".user").value = ""
     }
 })
-
 async function ShowUser(userName) {
-
     const user = users.find((user) => user.login === userName);
-
     if (typeof user === "undefined") {
-
         console.log("Usuário não encontrado");
-
     } else {
-
         const response = await fetch(user.repos_url);
         const repositorio = await response.json();
-
         repositorio.map((repo) => {
             // componentes para ser preenchidos
 
-
             let repoUser = document.createElement("div")
             repoUser.innerHTML = `
-            <h2>${repo.name}</h2>
-            <h3>${repo.description}</h3>
-            <p>${repo.stargazers_count}</p>
-            <p>${repo.fork}</p>
+            <h2>Nome: ${repo.name}</h2>
+            <h3>Descrição: ${repo.description}</h3>
+            <p>Número de estrelas: ${repo.stargazers_count}</p>
+            <p>Fork: ${repo.fork}</p>
             <hr>`
             document.querySelector(".card-repo").append(repoUser)
 
